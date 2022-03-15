afb892c563b9117505aad2a8e99af5ea60e3705e
19138350282 <2832879377@qq.com>
2022-03-15 17:00:01 +0800

    Initial commit

---
+++ src/main/java/com/example/refuse/controller/RefuseController.java
@@ -0,0 +1,20 @@
+package com.example.refuse.controller;
+
+import com.example.refuse.entity.RefuseInfo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @Description: TODO
+ * @author: scott
+ * @date: 2022年03月13日 15:53
+ */
+@RestController
+public class RefuseController {
+
+//    @GetMapping("/recyclable")
+//    public List<RefuseInfo>
+
+}
---
+++ src/main/java/com/example/refuse/result/RecyclableResult.java
@@ -0,0 +1,12 @@
+package com.example.refuse.result;
+
+/**
+ * @Description: TODO
+ * @author: scott
+ * @date: 2022年03月14日 16:52
+ */
+public class RecyclableResult {
+    private Integer code;
+    private String msg;
+    
+}
