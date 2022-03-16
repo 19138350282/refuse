e0f84b7c1c2a7b86d0330c27987fb7a3d7a25fa0
19138350282 <2832879377@qq.com>
2022-03-16 21:12:09 +0800

    refuse information

--- src/main/java/com/example/refuse/controller/RefuseController.java
+++ src/main/java/com/example/refuse/controller/RefuseController.java
@@ -1,6 +1,11 @@
 package com.example.refuse.controller;
 
 import com.example.refuse.entity.RefuseInfo;
+import com.example.refuse.mapper.RefuseInfoMapper;
+import com.example.refuse.result.RecyclableResult;
+import com.example.refuse.vo.Page;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +18,28 @@ import java.util.List;
  */
 @RestController
 public class RefuseController {
+    @Autowired
+    RefuseInfoMapper refuseInfoMapper;
 
-//    @GetMapping("/recyclable")
-//    public List<RefuseInfo>
+    @GetMapping("/findAll1")
+    public List<RefuseInfo> findAll1(){
+        return refuseInfoMapper.getrecyclable(1);
+    }
+
+    @GetMapping("/recyclable")
+    public RecyclableResult recyclable(@Param("refuse_id") Integer refuse_id,@Param("pagesize") Integer pagesize, @Param("pagenum") Integer pagenum){
+        String pageNum = Integer.toString(pagenum+1);
+        List<RefuseInfo> list = refuseInfoMapper.findByPage1(refuse_id,pagesize,pagenum);
+        Page<RefuseInfo> page=new Page<>();
+        page.setData(list);
+        int total = refuseInfoMapper.getTotal(refuse_id);
+        page.setTotal(total);
+        page.setPagenum(pageNum);
+        if(list.size()==0){
+            return new RecyclableResult(1,"未找到相关信息",null);
+        }else{
+            return new RecyclableResult(0,null,page);
+        }
+    }
 
 }
--- src/main/java/com/example/refuse/mapper/RefuseInfoMapper.java
+++ src/main/java/com/example/refuse/mapper/RefuseInfoMapper.java
@@ -1,8 +1,16 @@
 package com.example.refuse.mapper;
 
+import com.example.refuse.entity.RefuseInfo;
+import com.example.refuse.vo.Page;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface RefuseInfoMapper {
+    List<RefuseInfo> getrecyclable(Integer catenum);
 
+    int getTotal(Integer catenum);
+
+    List<RefuseInfo> findByPage1(Integer catenum, Integer pagesize, Integer pagenum);
 }
